eeaa65b3c1c9cf637251747bba0647020c68e704
Sphinx500 <1719210003@utectulancingo.edu.mx>
2022-05-18 17:28:15 -0500

    Uso de Logs

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/bean/MyBeanWithDependencyImplement.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/bean/MyBeanWithDependencyImplement.java
@@ -1,7 +1,12 @@
 package com.fundamentosplatzi.springboot.fundamentos.bean;
 
+import com.fundamentosplatzi.springboot.fundamentos.FundamentosApplication;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+
 public class MyBeanWithDependencyImplement implements  MyBeanWithDependence{
 
+    private final Log LOGGER = LogFactory.getLog(MyBeanWithDependencyImplement.class);
     private MyOperation myOperation;
 
     public MyBeanWithDependencyImplement(MyOperation myOperation) {
@@ -10,8 +15,10 @@ public class MyBeanWithDependencyImplement implements  MyBeanWithDependence{
 
     @Override
     public void printWithDependency() {
+        LOGGER.info("Hemos ingresado al metodo printWithDependency");
         int numero = 1;
         System.out.println(myOperation.sum(numero));
+        LOGGER.debug("El numero enviado como parametro es:" + numero);
         System.out.println("Hola desde la implementacion de un bean con dependencia");
     }
 }
